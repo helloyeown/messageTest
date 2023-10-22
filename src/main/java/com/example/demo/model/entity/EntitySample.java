95ec7ad9539fb909327ee897ad3046f05f0dc6eb
hiYewonMoon1 <toquasimodox@gmail.com>
2023-10-22 17:49:24 +0900

    h2db와 연결해서 auto ddl (USERS table)

--- src/main/java/com/example/demo/model/entity/EntitySample.java
+++ src/main/java/com/example/demo/model/entity/EntitySample.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@Table(name = "USER")
+@Table(name = "USERS")
 @EntityListeners(AuditingEntityListener.class)
 public class EntitySample {
 
